e04ac2c163511e83074d0026648ed40dc2bedc8a
Atsushi Watanabe <atsushi.w@ieee.org>
2018-02-22 00:02:55 +0900

    Use TRAVIS_PULL_REQUEST_SLUG instead of TRAVIS_REPO_SLUG

--- gh-pr-comment.sh
+++ gh-pr-comment.sh
@@ -12,9 +12,9 @@ function gh-pr-comment()
   then
     echo "usage: gh-pr-comment title comment" >&2
     echo "  env:" >&2
-    echo "     - TRAVIS_REPO_SLUG        : user/repos" >&2
-    echo "     - TRAVIS_PULL_REQUEST     : pull request number" >&2
-    echo "     - TRAVIS_BOT_GITHUB_TOKEN : token with comment write permission" >&2
+    echo "     - TRAVIS_PULL_REQUEST_SLUG : owner/repos" >&2
+    echo "     - TRAVIS_PULL_REQUEST      : pull request number" >&2
+    echo "     - TRAVIS_BOT_GITHUB_TOKEN  : token with comment write permission" >&2
     echo ' note: the script escapes special charactors for json encoding.' >&2
 
     return
@@ -24,7 +24,7 @@ function gh-pr-comment()
   then
     text=`echo "$2" | sed 's/\\\\/\\\\\\\\/g' | sed -n '1h;1!H;${x;s/\n/\\\\n/g;p;}' | sed 's/\"/\\\\"/g' | sed 's/\t/\\\\t/g' | sed 's/\//\\\\\//g'`
     curl -vs -X POST -H 'Content-Type:application/json' -d "{\"body\":\"## $1\n\n$text\"}" \
-      https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments?access_token=${TRAVIS_BOT_GITHUB_TOKEN} 2> /dev/null
+      https://api.github.com/repos/${TRAVIS_PULL_REQUEST_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments?access_token=${TRAVIS_BOT_GITHUB_TOKEN} 2> /dev/null
   else
     echo 'gh-pr-comment: TRAVIS_PULL_REQUEST is false.' >&2
   fi
