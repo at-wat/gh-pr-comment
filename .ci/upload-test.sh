d21ba7cef8ed7cb2eb4b3515911a219147622081
Atsushi Watanabe <atsushi.w@ieee.org>
2020-08-03 02:29:13 +0900

    Fix line break

--- .ci/upload-test.sh
+++ .ci/upload-test.sh
@@ -6,6 +6,7 @@ file=$(mktemp)
 echo "Test file" > ${file}
 ALLOW_PUBLIC_UPLOADER=true IMAGE_UPLOADER=test gh-pr-upload ${file} \
   && upload=OK || upload=Failed
+echo
 
 gh-pr-comment "Test post" \
   "testing comment post
