57666ed33352f04092659963f5f3d14ae930ddd0
Atsushi Watanabe <atsushi.w@ieee.org>
2020-08-03 15:04:04 +0900

    Add helper script to set docker env (#35)
    * Add helper script to set docker environment
    
    * Add README

---
+++ env.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Output list of docker run options to pass required environment variables.
+
+if [ "${TRAVIS}" = "true" ]
+then
+  echo "-e TRAVIS"
+  echo "-e TRAVIS_PULL_REQUEST"
+  echo "-e TRAVIS_PULL_REQUEST_SLUG"
+fi
+
+if [ -n "${GITHUB_API_URL_BASE}" ]
+then
+  echo "-e GITHUB_API_URL_BASE"
+fi
+
+if [ -n "${GITHUB_TOKEN}" ]
+then
+  echo "-e GITHUB_TOKEN"
+fi
+
+if [ -n "${TRAVIS_BOT_GITHUB_TOKEN}" ]
+then
+  echo "-e TRAVIS_BOT_GITHUB_TOKEN"
+fi
