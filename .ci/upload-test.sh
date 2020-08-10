1d90d8a519e6b35ce302faa33d8c0585e0d3452e
Atsushi Watanabe <atsushi.w@ieee.org>
2020-08-10 20:38:12 +0900

    Post OS and arch

--- .ci/upload-test.sh
+++ .ci/upload-test.sh
@@ -8,7 +8,7 @@ ALLOW_PUBLIC_UPLOADER=true IMAGE_UPLOADER=test gh-pr-upload ${file} \
   && upload=OK || upload=Failed
 echo
 
-gh-pr-comment "Test post" \
+gh-pr-comment "Test post ${TRAVIS_OS_NAME} ${TRAVIS_CPU_ARCH}" \
   "testing comment post
 - UTF-8 text: \"bœuf/牛\"
 - upload test: ${upload}
