d710f7c82ecee679a8786aa00ecc9b99b1fb8148
Atsushi Watanabe <atsushi.w@ieee.org>
2020-08-11 09:13:46 +0900

    Support arm and darwin install (#45)

--- install.sh
+++ install.sh
@@ -3,12 +3,18 @@
 set -eu
 
 arch=
-case $(uname -p) in
+case $(uname -m) in
   x86_64)
     arch=amd64
     ;;
+  arm64)
+    arch=arm64
+    ;;
+  aarch64)
+    arch=arm64
+    ;;
   *)
-    echo "unsupported arch" >&2
+    echo "unsupported arch $(uname -m)" >&2
     exit 1
     ;;
 esac
@@ -20,11 +26,22 @@ case $(uname -s) in
     os=linux
     ext=.tar.gz
     ;;
+  Darwin)
+    os=darwin
+    ext=.tar.gz
+    ;;
   *)
-    echo "unsupported OS" >&2
+    echo "unsupported OS $(uname -s)" >&2
     exit 1
     ;;
 esac
+
+# Check required commands.
+if ! which curl > /dev/null 2> /dev/null
+then
+  echo "curl not found" >&2
+  exit 1
+fi
 
 api_auth=
 if [ -n "${GITHUB_TOKEN:-${TRAVIS_BOT_GITHUB_TOKEN:-}}" ]
@@ -35,21 +52,28 @@ fi
 gh_api_base=${GITHUB_API_URL_BASE:-https://api.github.com}
 
 tag=${1:-latest}
-rel=
-if [ ${tag} = "latest" ]
+ep=latest
+if [ "${tag}" != "latest" ]
 then
-  rel=$(eval curl \
-    ${api_auth} \
-    -s --retry 4 \
-    ${gh_api_base}/repos/at-wat/gh-pr-comment/releases/latest)
-else
-  rel=$(eval curl \
-    ${api_auth} \
-    -s --retry 4 \
-    ${gh_api_base}/repos/at-wat/gh-pr-comment/releases/tags/${tag})
+  ep=tags/${tag}
 fi
 
-url=$(echo "${rel}" | sed -n 's/.*"browser_download_url":\s*"\([^"]*\)"/\1/p' | grep "_${os}_${arch}${ext}" | head -n1)
+curl_err_file=$(mktemp)
+rel=$(eval curl \
+  ${api_auth} \
+  -sSL --retry 4 \
+  ${gh_api_base}/repos/at-wat/gh-pr-comment/releases/${ep} 2> ${curl_err_file} || true)
+curl_err="$(cat ${curl_err_file})"
+rm -f ${curl_err_file}
+if [ -n "${curl_err}" ]
+then
+  echo "failed to fetch releases: ${curl_err}" >&2
+  exit 1
+fi
+
+url=$(echo "${rel}" \
+  | sed -n 's/.*"browser_download_url":[ \t\r\n]*"\([^"]*\)"/\1/p' \
+  | grep -e "_${os}_${arch}${ext}$" | head -n1)
 echo ${url} 
 if [ -z "${url}" ]
 then
@@ -59,7 +83,7 @@ fi
 
 tmpdir=$(mktemp -d)
 dir=${2:-~/.local/bin/}
-curl -sL ${url} | tar xzfv - -C ${tmpdir}/
+curl -sSL ${url} | tar xzfv - -C ${tmpdir}/
 
 mkdir -p ${dir}
 cp ${tmpdir}/gh-pr-comment ${dir}
