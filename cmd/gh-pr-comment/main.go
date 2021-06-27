e627b358eec9317286f29f040ff469a9e4861341
Atsushi Watanabe <atsushi.w@ieee.org>
2021-06-27 16:54:18 +0900

    Update import path

--- cmd/gh-pr-comment/main.go
+++ cmd/gh-pr-comment/main.go
@@ -9,7 +9,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/google/go-github/v35/github"
+	"github.com/google/go-github/v36/github"
 	"golang.org/x/oauth2"
 
 	"github.com/at-wat/gh-pr-comment/pkg/cienv"
