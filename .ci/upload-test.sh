005f3c7b30bfe9230e68f81c69e30dcc5cc12eff
Atsushi Watanabe <atsushi.w@ieee.org>
2025-05-14 13:20:23 +0900

    Test uploader only on linux

--- .ci/upload-test.sh
+++ .ci/upload-test.sh
@@ -16,23 +16,26 @@ ALLOW_PUBLIC_UPLOADER=true \
     || upload_test=Failed
 echo
 
-echo "Testing s3 uploader"
-IMAGE_UPLOADER=s3 \
-  AWS_REGION=ap-northeast-1 \
-  AWS_S3_BUCKET=test-bucket \
-  AWS_ENDPOINT_URL=http://localhost:4566 \
-  AWS_S3_USE_PATH_STYLE=true \
-  AWS_ACCESS_KEY_ID=test \
-  AWS_SECRET_ACCESS_KEY=test \
-  gh-pr-upload ${file} \
-    && upload_s3=OK \
-    || upload_s3=Failed
+if [ "$(uname -o)" = "GNU/Linux" ]; then
+  echo "Testing s3 uploader"
+  IMAGE_UPLOADER=s3 \
+    AWS_REGION=ap-northeast-1 \
+    AWS_S3_BUCKET=test-bucket \
+    AWS_ENDPOINT_URL=http://localhost:4566 \
+    AWS_S3_USE_PATH_STYLE=true \
+    AWS_ACCESS_KEY_ID=test \
+    AWS_SECRET_ACCESS_KEY=test \
+    gh-pr-upload ${file} \
+      && upload_s3=OK \
+      || upload_s3=Failed
+fi
 
 rm ${file}
 
 gh-pr-comment "✔ $1" "test uploader: ${upload_test}
-s3 uploader: ${upload_s3}
+s3 uploader: ${upload_s3:-n/a}
 $(go version)
 "
 
-[ ${upload_test} = "OK" ] && [ ${upload_s3} = "OK" ]
+[ ${upload_test} = "OK" ]
+[ ${upload_s3:-OK} = "OK" ]
